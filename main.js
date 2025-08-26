2aa66f94be11e108d6f8671ba0cf92fc7ace2d8c
Aurel <aurel@outlook.com>
2025-08-26 09:22:32 +0200

    add

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch('https://ingrwf12.cepegra-frontend.xyz/cockpit1/api/content/item/voyages')
+.then(r => r.json())
+.then(r => {
+  console.log(r)
+  document.querySelector('.voyage').innerHTML = r['voyages-label']
+  document.querySelector('.description').innerHTML = r['voyages-description']
+  document.querySelector('.prix').innerHTML = r['voyages-prix']
+})
