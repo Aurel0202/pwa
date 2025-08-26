0e5339bc7f0af7e63b2e828c0ea93bc94bc9cd5b
Aurel <aurel@outlook.com>
2025-08-26 11:08:14 +0200

    add

---
+++ install.js
@@ -0,0 +1,24 @@
+const installBtn = document.querySelector('.install-btn')
+let deferredPrompt = null
+
+installBtn.classList.add('hidden')
+
+window.addEventListener('beforeinstallprompt', e => {
+  e.preventDefault()
+  deferredPrompt = e
+  installBtn.classList.remove('hidden')
+})
+
+installBtn.addEventListener('click', async e => {
+  e.preventDefault()
+  deferredPrompt.prompt()
+  const userChoice = await deferredPrompt.userChoice
+  if(user == "accepted") {
+    installBtn.classList.add('hidden')
+  }
+  deferredPrompt = null
+})
+
+window.addEventListener('appinstalled', e => {
+  installBtn.classList.add('hidden')
+})
