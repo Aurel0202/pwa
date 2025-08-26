c65288b834713a8ade50f7dd3bc40778a40e5c1a
Aurel <aurel@outlook.com>
2025-08-26 16:21:19 +0200

    add notif

--- notification.js
+++ notification.js
@@ -1,30 +1,33 @@
 const notification = () => {
-  const notifBtn = document.querySelector('.notif-me')
-  notifBtn.addEventListener('click', e => {
-    e.preventDefault()
-    let myNotification = null
+  const notifBtn = document.querySelector(".notif-me");
+  notifBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let myNotification = null;
 
     const options = {
       body: "Texte de la notif",
       icon: "/icons/favicon-16x16.png",
       url: "https://cepegra.be",
-      vibrate: [200,100,200,100,200,100,200]
-    }
-    if(!("Notification" in window)) {
-      alert('Pas de notifs dispos dans ce navigateur')
-    } else if (Notification.permission === 'granted') {
-      console.log('Notif accepted')
-      myNotification = new Notification("Hello", options)
+      vibrate: [200, 100, 200, 100, 200, 100, 200],
+    };
+    if (!("Notification" in window)) {
+      alert("Pas de notifs dispos dans ce navigateur");
+    } else if (Notification.permission === "granted") {
+      console.log("Notif accepted 1");
+      myNotification = new Notification("Hello", options);
     } else {
-      Notification.requestPermission()
-      .then(permission => {
-        if(permission === 'granted') {
-          console.log('Notif accepted')
-          myNotification = new Notification("Hello", options)
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("Notif accepted 2");
+          myNotification = new Notification("Hello", options);
         }
-      })
+      });
     }
-  })
-}
+    myNotification.onclick = () => {
+      window.open("https://cepegra.be", "_blank");
+    };
+    myNotification = null;
+  });
+};
 
-notification()
+notification();
